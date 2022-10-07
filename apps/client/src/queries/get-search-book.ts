22577c5b6cd6f9291f8b1f85922846a50ce13d35
bichikim <bichi@live.co.kr>
2022-10-07 09:54:35 +0900

    update

--- apps/client/src/queries/get-search-book.ts
+++ apps/client/src/queries/get-search-book.ts
@@ -8,15 +8,41 @@ export interface GetSearchBookPayLoad {
   size?: number
 }
 
+export interface BookType {
+  authors: string[]
+  contents: string
+  datetime: string
+  isbn: string
+  price: number
+  publisher: string
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  sale_price: number
+  status: string
+  thumbnail: string
+  title: string
+  translators: string[]
+  url: string
+}
+
+export interface GetSearchBookMeta {
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  total_count?: number
+}
+
+export interface GetSearchBookResponse {
+  documents?: BookType[]
+  meta?: GetSearchBookMeta
+}
+
 export const useGetSearchBook = (payload: GetSearchBookPayLoad) => {
   const {page, size, search} = payload
   const axios = useAxios()
 
   const query = useQuery(
     ['get-search-book'],
-    async () => {
+    async (): Promise<GetSearchBookResponse> => {
       if (!search) {
-        return
+        return {}
       }
       const {data} = await axios.get('/v3/search/book', {
         params: {
