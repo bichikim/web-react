0664e6ec033427b022d1fdec01c07857580689ca
bichi <bichi@live.co.kr>
2026-04-08 15:57:08 +0900

    Refactor useOnce hook to accept both a value and a function, ensuring it retains the initial value across re-renders; add documentation in Korean for clarity.

--- packages/react-hooks/src/use-once/index.ts
+++ packages/react-hooks/src/use-once/index.ts
@@ -1,5 +1,11 @@
 import {useState} from 'react'
 
-export const useOnce = <T>(handle: () => T): T => {
-  return useState(handle)[0]
+/**
+ * 한번만 값을 가져오고 (함수면 한번만 실행) 리렌더링을 하더라고 처음 값을 유지 합니다
+ * @param value
+ * @returns
+ */
+export const useOnce = <T>(value: T | (() => T)): T => {
+  const [onceValue] = useState(value)
+  return onceValue
 }
